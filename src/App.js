3ff1eb3fa2520232269cc5c39f758decd828c9f0
pruthvirajmv <74802690+pruthvirajmv@users.noreply.github.com>
2021-02-21 01:27:32 +0530

    feat: responsive cart made cart list responsive

--- src/App.js
+++ src/App.js
@@ -2,35 +2,81 @@ import "./styles.css";
 import React, { useState } from "react";
 
 function AddToCart() {
-  const [add, setAdd] = useState({ Tshirt: 0, Trousers: 0, Shoes: 0 });
+  const [cartList, setCartList] = useState([]);
 
-  var cartList = [
-    { name: "Tshirt"},
-    { name: "Trousers"},
-    { name: "Shoes"}
+  const productList = [
+    { name: "Tshirt", count: 1 },
+    { name: "Trousers", count: 1 },
+    { name: "Shoes", count: 1 }
   ];
+
+  function cartUpdate(select) {
+    if (cartList.some((item) => item.name === select.name)) {
+      let update = cartList.reduce((cart, item) => {
+        if (item.name === select.name) {
+          if (item.count > 0) cart.push({ ...item, count: item.count + 1 });
+        } else {
+          cart.push(item);
+        }
+        return cart;
+      }, []);
+      setCartList(update);
+    } else {
+      cartList.push(select);
+      setCartList([...cartList]);
+    }
+  }
+
+  function removeItem(select) {
+    if (cartList.some((item) => item.name === select.name)) {
+      let update = cartList.reduce((cart, item) => {
+        // console.log(cart);
+        // console.log(item);
+        if (item.name === select.name) {
+          if (item.count > 1) cart.push({ ...item, count: item.count - 1 });
+        } else {
+          console.log(item);
+          cart.push(item);
+        }
+        //console.log(cart);
+        return cart;
+      }, []);
+      console.log(update);
+      setCartList(update);
+    }
+  }
+  //console.log(cartList);
 
   return (
     <>
-    <div>
-      <ul>
-        {cartList.map((item) => (
-          <li key={item.name}> {item.name}
-          <button onClick ={() =>
-        setAdd({ ...add, [item.name]: add.[item.name]+1 })  
-        }>add</button> </li>
-        ))}
-      </ul>
+      <div>
+        <h1> Proucts </h1>
+        <ul>
+          {productList.map((item) => (
+            <li key={item.name}>
+              {" "}
+              {item.name}
+              <button onClick={() => cartUpdate(item)}>Add</button>
+            </li>
+          ))}
+        </ul>
+
+        <h1> Your Cart </h1>
 
         <ul className="cart">
-        {cartList.map((item) => (
-          <li key={item.name}>
-            {" "}
-            {item.name} <div>Qty in Cart - {add.[item.name]}</div>{" "}
-          </li>
-        ))}
+          {cartList.map((item) => (
+            <li key={item.name}>
+              {" "}
+              {item.name}{" "}
+              <div>
+                Qty in Cart -{" "}
+                <button onClick={() => removeItem(item)}>-</button> {item.count}{" "}
+                <button onClick={() => cartUpdate(item)}>+</button>
+              </div>{" "}
+            </li>
+          ))}
         </ul>
-    </div>
+      </div>
     </>
   );
 }
